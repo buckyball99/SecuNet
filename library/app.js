985244cb0610cacd35f0ed99d7950ebc2cb87500
= <=>
2019-02-17 14:26:56 +0530

    Dependency bodyparser.Redirect added

--- library/app.js
+++ library/app.js
@@ -1,12 +1,18 @@
 var express= require("express");
 var config = require('./config');
+var bodyParser = require("body-parser");
 
 var app = express();
 console.log("OUR EXPRESS APP WILL GO HERE!!");
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));   // key-value pairs used with {}
 app.set("view engine","ejs");
 
+
+var LoginInfo = [
+    "Tony","Edward","Justin","Rose", "DEEZNUTS"
+]
 
 //Get functions here
 app.get("/",function(request,response){     //Req contains all the information about the request that was made that triggered this route                                            // Res contains the information about what we are going to respond with
@@ -18,11 +24,25 @@ app.get("/fallInLoveWithUsername/:thing",function(req,res){
     res.render("love",{thingVar:thing});
 });
 app.get("/Login",function(req,res){
-    res.send("This is the correct login id");
+    // var LoginInfo = [
+    //     "Tony","Edward","Justin","Rose", "DEEZNUTS"
+    // ]
+    // res.send("This is the correct login id");
+    res.render("info",{LoginInfo:LoginInfo});
 });
 app.get("/Password",function(req,res){
     res.send("This is the correct Password");
 });
+
+app.post("/addUser",function(req,res){
+    var newUser = req.body.newUser;
+    LoginInfo.push(newUser);
+    console.log(req.body);
+    // res.send("You have reached the post route");
+    res.redirect('/Login');
+
+})
+
 app.get("/speak/:animal",function(req,res){
     var animal = req.params.animal.toLowerCase();
     var soundDict = {
